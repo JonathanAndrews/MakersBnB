6cfae847fd45ac1fcde14b7a0e57d8fb8668902d
Alex <39955296+Kharouk@users.noreply.github.com>
2018-10-10 14:58:18 +0100

    Implemented Signup request and routes (#18)
    * fix rootPage bug in assert text
    
    * add form to landing page and write feature tests
    
    * complete feature test and sign up form requests
    
    * add signup routes and fix issue page
    
    * refactor index

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 const port = 3000;
 
 // Connecting to our Public folder
 app.use(express.static('public'));
+
+// Setting up Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'this-is-secretive-shhhh' }));
 
 // Setting our View Engine
 app.set('view engine', 'ejs');
@@ -14,7 +21,16 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/signup', (req, res) => {});
+app.post('/signup', (req, res) => {
+  req.session.email = req.body['sign-up-email'];
+  req.session.password = req.body['sign-up-password'];
+  res.redirect('/dashboard');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.locals.email = req.session.email;
+  res.render('dashboard');
+});
 
 // Connecting to our localhost
 app.listen(port, () => {
--- spec/units/userSpec.js
+++ spec/units/userSpec.js
@@ -1,15 +1,9 @@
 const User = require('../../src/user');
 
 describe('User', () => {
-  xit('should be able to find a user', () => {
-    console.log('1');
-    const recoveredUser = User.find({ email: 'alex@mail.com' }, (err, users) => {
-      console.log('4');
-      return users;
+  it('should be able to find a user', () => {
+    User.find({}, (err, users) => {
+      expect(users).toEqual('mathilda@email.com');
     });
-    console.log('2');
-    console.log(recoveredUser);
-    expect(recoveredUser.email).toEqual('jon@mail.com');
-    console.log('3');
   });
 });
